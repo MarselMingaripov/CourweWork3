29b53d14795e76037a61d81d683dd967567adba7
MarselMingaripov <bard1812@gmail.com>
2023-02-13 19:13:14 +0300

    initial commit

---
+++ src/test/java/ru/min/coursework3/CourseWork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.min.coursework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
